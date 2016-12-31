c4e88c5c89e6ddd54770d4156f853e0dc4d731e7
Nuno1123 <nuno.monteiro11@hotmail.com>
2016-12-31 18:31:59 +0000

    form-based workflow feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,16 +1,53 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   #routes.rb redirects the GET by the browser to this method which displays all
   #articles
   def index
-  @articles = Article.all  #@ before a variable indicates it is an instance variable, can be access from outside
+    @articles = Article.all  #@ before a variable indicates it is an instance variable, can be access from outside
   end
 
   #when we click an article link (title) it show be directed to the article which
   #is what this method does
   def show
     @article = Article.find(params[:id])
+  end
 
+  #to create a new article when we click the create article link_to
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    flash.notice = "Article '#{@article.title}' created"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    article_title = @article.title
+    @article.destroy
+
+    flash.notice = "Article '#{article_title}' deleted"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' updated"
+
+    redirect_to article_path(@article)
   end
 
 end
